cf9f197a5c4c9c1f255f3cbb5ca917778044a53d
anthavio <anthavio@post.cz>
2014-03-13 13:36:07 +0000

    better exception message

--- src/main/java/net/anthavio/httl/inout/ResponseBodyExtractors.java
+++ src/main/java/net/anthavio/httl/inout/ResponseBodyExtractors.java
@@ -1,13 +1,16 @@
 package net.anthavio.httl.inout;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.anthavio.httl.SenderResponse;
 import net.anthavio.httl.util.Cutils;
-import net.anthavio.httl.util.HttpHeaderUtil;
 import net.anthavio.httl.util.GenericType;
+import net.anthavio.httl.util.HttpHeaderUtil;
 
 /**
  * Storage for ResponseExtractor
@@ -112,6 +115,22 @@ public class ResponseBodyExtractors {
 		}
 	};
 
+	public static final ResponseBodyExtractor<Reader> READER = new ResponseBodyExtractor<Reader>() {
+
+		@Override
+		public Reader extract(SenderResponse response) throws IOException {
+			return response.getReader();
+		}
+	};
+
+	public static final ResponseBodyExtractor<InputStream> STREAM = new ResponseBodyExtractor<InputStream>() {
+
+		@Override
+		public InputStream extract(SenderResponse response) throws IOException {
+			return response.getStream();
+		}
+	};
+
 	/**
 	 * Extracts Response into desired resultType or fails miserably.
 	 * This method does NOT close Response
@@ -148,7 +167,7 @@ public class ResponseBodyExtractors {
 			return (ResponseBodyExtractor<T>) ResponseBodyExtractors.BYTES;
 		}
 
-		ResponseExtractorFactory extractorFactory = getFactory(response);
+		ResponseExtractorFactory extractorFactory = getFactory(response, clazz);
 		ResponseBodyExtractor<T> extractor = extractorFactory.getExtractor(response, clazz);
 		if (extractor == null) {
 			throw new IllegalStateException("ResponseExtractorFactory " + extractorFactory + " returned null");
@@ -157,7 +176,7 @@ public class ResponseBodyExtractors {
 	}
 
 	public <T> ResponseBodyExtractor<T> getExtractor(SenderResponse response, GenericType<T> typeReference) {
-		ResponseExtractorFactory extractorFactory = getFactory(response);
+		ResponseExtractorFactory extractorFactory = getFactory(response, typeReference.getParameterizedType());
 		ResponseBodyExtractor<T> extractor = extractorFactory.getExtractor(response, typeReference.getParameterizedType());
 		if (extractor == null) {
 			throw new IllegalStateException("ResponseExtractorFactory " + extractorFactory + " returned null");
@@ -165,7 +184,7 @@ public class ResponseBodyExtractors {
 		return extractor;
 	}
 
-	private ResponseExtractorFactory getFactory(SenderResponse response) {
+	private ResponseExtractorFactory getFactory(SenderResponse response, Type type) {
 		String contentType = response.getFirstHeader("Content-Type");
 		if (Cutils.isEmpty(contentType)) {
 			throw new IllegalArgumentException("Content-Type header not found");
@@ -173,7 +192,7 @@ public class ResponseBodyExtractors {
 		String mediaType = HttpHeaderUtil.getMediaType(contentType);
 		ResponseExtractorFactory extractorFactory = factories.get(mediaType);
 		if (extractorFactory == null) {
-			throw new IllegalArgumentException("Extractor factory not found for " + mediaType);
+			throw new IllegalArgumentException("ResponseExtractorFactory not found for " + mediaType + " to extract " + type);
 		}
 		return extractorFactory;
 	}
